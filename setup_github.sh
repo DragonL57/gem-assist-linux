70aa466155a70b17b7aedb96b59b112fd89d898d
DragonL57 <vmthelong2004@gmail.com>
2025-03-23 18:13:20 +0700

    Remove google_search and meta_search

--- setup_github.sh
+++ setup_github.sh
@@ -23,7 +23,7 @@ git add .
 
 # Commit changes
 echo -e "${BLUE}Committing changes...${NC}"
-git commit -m "Limit tool use for flask UI to prevent error"
+git commit -m "Remove google_search and meta_search"
 
 # Check if the remote exists
 REMOTE_EXISTS=$(git remote | grep origin)
